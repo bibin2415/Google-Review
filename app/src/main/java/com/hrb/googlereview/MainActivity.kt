2dc7f037ff45527edb48685e64e314d859a5857a
Bibin Baby <bibin.baby@hrblock.com>
2021-07-16 17:23:59 +0530

    Inapp update

--- app/src/main/java/com/hrb/googlereview/MainActivity.kt
+++ app/src/main/java/com/hrb/googlereview/MainActivity.kt
@@ -1,14 +1,26 @@
 package com.hrb.googlereview
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
+import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.play.core.appupdate.AppUpdateManager
+import com.google.android.play.core.appupdate.AppUpdateManagerFactory
+import com.google.android.play.core.install.InstallStateUpdatedListener
+import com.google.android.play.core.install.model.ActivityResult
+import com.google.android.play.core.install.model.AppUpdateType
+import com.google.android.play.core.install.model.AppUpdateType.IMMEDIATE
+import com.google.android.play.core.install.model.InstallStatus
+import com.google.android.play.core.install.model.UpdateAvailability
 import com.google.android.play.core.review.ReviewInfo
 import com.google.android.play.core.review.ReviewManager
 import com.google.android.play.core.review.ReviewManagerFactory
 import com.hrb.googlereview.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,6 +28,14 @@ class MainActivity : AppCompatActivity() {
     private val ACTIVITY_CALLBACK = 1
     private var reviewInfo: ReviewInfo? = null
     private lateinit var reviewManager: ReviewManager
+    private lateinit var appUpdateManager: AppUpdateManager
+
+    private val listener: InstallStateUpdatedListener? =
+        InstallStateUpdatedListener { installState ->
+            if (installState.installStatus() == InstallStatus.DOWNLOADED) {
+                showSnackBarForCompleteUpdate()
+            }
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,7 +56,7 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
-        Handler().postDelayed({
+        /*Handler().postDelayed({
             reviewInfo?.let {
                 val flow = reviewManager.launchReviewFlow(this@MainActivity, it)
                 flow.addOnSuccessListener {
@@ -52,7 +72,80 @@ class MainActivity : AppCompatActivity() {
                     //in production app.
                 }
             }
-        }, 3000)
+        }, 3000)*/
+        appUpdateManager = AppUpdateManagerFactory.create(this)
+        val appInfo = appUpdateManager.appUpdateInfo
+    }
+
+    private fun showSnackBarForCompleteUpdate() {
+        val snackbar = Snackbar
+            .make(
+                findViewById(R.id.parent),
+                "Update ready for install", Snackbar.LENGTH_LONG
+            )
+            .setAction("Install", View.OnClickListener {
+                appUpdateManager!!.completeUpdate()
+            })
+        snackbar.show()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if (listener != null) {
+            appUpdateManager.unregisterListener(listener)
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (listener != null) {
+            appUpdateManager.registerListener(listener)
+        }
+        appUpdateManager
+            .appUpdateInfo.addOnSuccessListener { appUpdateInfo ->
+                if (appUpdateInfo.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE
+                    && appUpdateInfo.isUpdateTypeAllowed(AppUpdateType.IMMEDIATE)
+                ) {
+                    appUpdateManager.startUpdateFlowForResult(
+                        appUpdateInfo,
+                        AppUpdateType.IMMEDIATE,
+                        this,
+                        100
+                    )
+                } else if (appUpdateInfo.updateAvailability()
+                    == UpdateAvailability.DEVELOPER_TRIGGERED_UPDATE_IN_PROGRESS
+                ) {
+                    appUpdateManager.startUpdateFlowForResult(
+                        appUpdateInfo,
+                        IMMEDIATE,
+                        this,
+                        100
+                    );
+                }
+            }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        // super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 100) {
+            when (resultCode) {
+                Activity.RESULT_OK -> {
+                    Toast.makeText(this, "RESULT_OK", Toast.LENGTH_LONG).show()
+                }
+                Activity.RESULT_CANCELED -> {
+                    Toast.makeText(this, "RESULT_CANCELED", Toast.LENGTH_LONG).show()
+                    finish()
+                }
+                ActivityResult.RESULT_IN_APP_UPDATE_FAILED -> {
+                    Toast.makeText(this, "RESULT_IN_APP_UPDATE_FAILED", Toast.LENGTH_LONG).show()
+                    finish()
+                }
+                else -> {
+                    Toast.makeText(this, "No result", Toast.LENGTH_LONG).show()
+                }
+            }
+        }
     }
 
 
